7c6a4a3a7d80efac3f7ef4f21aa6f7adf7564e61
muluante <muluante@outlook.com>
2022-11-29 02:54:50 -0800

    101-remove_char_at

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
 def remove_char_at(str, n):
-        if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+    newStr = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            newStr += str[i]
+    return newStr
