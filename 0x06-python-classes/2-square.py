3439d49e32675137ddcec8843f15ca5c3f9cfee8
muluante <muluante@outlook.com>
2022-12-20 03:44:47 -0800

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
+"""A class Square that defines a square
+
+by: (based on 1-square.py)
+"""
 
 class Square:
-
-    """square"""
+    """Square class with a private attribute
+    size.
+    """
 
     def __init__(self, size=0):
-
-        """
-
-        initialize square size
-
+        """Initializes the size variable as a private
+        instance artribute
         Args:
-
-        size (int): size of square
-
-        Returns:
-
-        None
-
+            __size (int): The __size of the new square.
         """
-
-
-
-        if type(size) is not int:
-
-            raise TypeError("size must be an integer")
-
-        elif size < 0:
-
-            raise ValueError("size must be >= 0")
-
         self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
