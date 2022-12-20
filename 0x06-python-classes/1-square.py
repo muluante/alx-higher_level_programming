a07283cd315a44303abfcb15663d1ef453d0bec1
muluante <muluante@outlook.com>
2022-12-20 03:35:48 -0800

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
+Square = __import__('1-square').Square
 
-class Square:
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
 
-    """square"""
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
 
-    def __init__(self, size):
-
-        """
-
-        initialize square attributes
-
-        Args:
-
-        size (int): size of square
-
-        Returns:
-
-        None
-
-        """
-
-
-
-        self.__size = size
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
