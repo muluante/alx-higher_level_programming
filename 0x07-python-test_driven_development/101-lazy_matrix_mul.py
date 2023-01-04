b411dda0afe723da03b20139cfbd3b0e9ccf3b98
muluante <muluante@outlook.com>
2023-01-03 22:19:46 -0800

    101-lazy_matrix_mul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
+"""Module lazy_matrix_mul
+Matrix multiplication using NumPy.
 """
-This is the lazy_matrix_mul module.
-This module supplies one function, lazy_matrix_mul().
-"""
-import numpy as np
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """Multiplies m_a and m_b using
+    matmul, and returns the result.
     """
-    Return a new matrix where each element has been divided by div.
-    Args:
-        m_a (list): list of lists of integers or floats.
-        m_b (list): list of lists of integers or floats.
-    """
-
-    return np.dot(m_a, m_b)
+    return numpy.matmul(m_a, m_b)
