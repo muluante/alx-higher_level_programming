2e3fbdff00f64d663a6919cd25f16b5b6766c8e4
muluante <muluante@outlook.com>
2023-01-09 07:11:47 -0800

    11-square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,60 +1,36 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """creates a class BaseGeometry"""
-    def area(self):
-        """calculate the area
-        Raise:
-            Exception: area is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates the value
-        Args:
-            name(str): name
-            value(int): value
-        Raises:
-            TypeError: if value is not type int
-            ValueError: if value is less than or equal to 0
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+"""
+Geometry module
+"""
 
 
-class Rectangle(BaseGeometry):
-    """creates a subclass Rectangle of class BaseGeometry"""
-    def __init__(self, width, height):
-        """initialize attributes
-        Args:
-            width(int): width of Rectangle
-            height(int): height of Rectangle
-        """
-        BaseGeometry.__init__(self)
-        self.__width = width
-        self.__height = height
-        super().integer_validator("width", self.__width)
-        super().integer_validator("height", self.__height)
-
-    def area(self):
-        """calculates area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """prints string"""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """creates subclass Square"""
+    """
+    A class inheriting from the Rectangle class which inherits
+    from the BaseGeometry class
+    Private instance attributes:
+        - width
+        - height
+    """
     def __init__(self, size):
-        """initialize instance attributes"""
-        self.__size = size
-        super().integer_validator("size", self.__size)
+        """
+        Class constructor with private attributes
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
-        super().area()
+        self.__size = size
+
+    def area(self):
+        """
+        Method to calculate area
+        """
+        return self.__size ** 2
 
     def __str__(self):
-        """print string"""
-        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+        """
+        Returns printable representation of the square instance
+        """
+        return str("[Square] {}/{}".format(self.__size, self.__size))
