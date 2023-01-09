3673616276bc97ca4dcff99fc3f9d605ee5deb74
muluante <muluante@outlook.com>
2023-01-08 23:53:00 -0800

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """creates a class BaseGeometry"""
+    def area(self):
+        """calculate the area
+        Raise:
+            Exception: area is not implemented
+        """
+        raise Exception("area() is not implemented")
