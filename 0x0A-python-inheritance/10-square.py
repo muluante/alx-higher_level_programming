49c1f2e7646d63f657f81fdb29248e3efc252078
muluante <muluante@outlook.com>
2023-01-09 07:10:07 -0800

    10-square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""superclass Rectangle"""
 
 
 class Square(Rectangle):
-    """creates subclass Square"""
+    """
+    A class inheriting from the Rectangle class which inherits
+    from the BaseGeometry class
+    Private instance attributes:
+        - width
+        - height
+    """
     def __init__(self, size):
-        """initialize instance attributes
-        Args:
-            size (int): size of square
+        """
+        Class constructor with private attributes
         """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """
+        Method to calculate area
+        """
+        return self.__size ** 2
