81858645ce09c783ae4a9003d80d3fdb15663944
muluante <muluante@outlook.com>
2023-01-10 02:30:19 -0800

    7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Add item script."""
+"""
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
+
 import sys
+import json
+import os.path
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+my_file = 'add_item.json'
 
-argc = len(sys.argv)
+my_list = []
 
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
 
-save_to_json_file(lst, "add_item.json")
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
