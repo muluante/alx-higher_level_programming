b9adea3edeea661f66a620846be2dfacef159a1a
muluante <muluante@outlook.com>
2023-01-10 02:12:32 -0800

    7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""Module 11-student.
-Creates a Student class.
+"""Module 9-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
 """
 
+import sys
+import json
+import os.path
 
-class Student:
-    """Class that defines a student.
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json().
-    """
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-    def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
+my_file = 'add_item.json'
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+my_list = []
 
-    def to_json(self):
-        """Retrieves a dictionary representation
-        of a Student instance.
-        Returns: the dict representation of the instance.
-        """
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
 
-        return self.__dict__
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
