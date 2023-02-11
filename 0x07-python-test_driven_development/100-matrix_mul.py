27263f28e2d3d643e23724f438fd4f5d2edad339
muluante <muluante@outlook.com>
2023-02-11 05:48:44 -0800

    100-matrix_mul

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,64 +1,59 @@
 #!/usr/bin/python3
 """
-This is the matrix_mul module.
-This module supplies one function, matrix_mul().
+Multiplies two matrices
 """
 
 
 def matrix_mul(m_a, m_b):
-    """
-    Return a new matrix multiplied.
+    """multiplies two matrices
     Args:
-        m_a (list): list of lists of integers or floats.
-        m_b (list): list of lists of integers or floats.
+        m_a (:obj:`list` of :obj:`list` of :obj:`int` or :obj:`float`):
+            list of lists of integers or floats
+        m_b (:obj:`list` of :obj:`list` of :obj:`int` or :obj:`float`):
+            list of lists of integers or floats
+    Returns:
+        a new matrix containing dot products
+    Raises:
+        TypeError: if m_a or m_b is not a list, if one element of those
+            two lists is not an integer or a float,
+            if m_a and m_b are not rectangular
+        ValueError: if m_a or m_b is empty, if m_a or m_b can't be multiplied
     """
+    new = []
+    dp = 0
+    flag = 1
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-
-    for row_a in m_a:
-        if type(row_a) is not list:
-            raise TypeError("m_a must be a list of lists")
-    for row_b in m_b:
-        if type(row_b) is not list:
-            raise TypeError("m_b must be a list of lists")
-
-    if len(m_a) > 0:
-        for row_a in m_a:
-            if len(row_a) is 0:
-                raise ValueError("m_a can't be empty")
-    else:
+    if m_a is None:
         raise ValueError("m_a can't be empty")
-    if len(m_b) > 0:
-        for row_b in m_b:
-            if len(row_b) is 0:
-                raise ValueError("m_b can't be empty")
-    else:
+    if m_b is None:
         raise ValueError("m_b can't be empty")
 
-    for row_a in m_a:
-        for i in row_a:
-            if type(i) not in [int, float]:
+    for i in range(len(m_a)):
+        inside = []
+        for j in range(len(m_b[i])):
+            if len(m_a[i]) != len(m_b):
+                raise ValueError("m_a and m_b can't be multiplied")
+            if type(m_a[i]) is not list and type(m_a[i][j]) is not int\
+               and type(m_a[i][j]) is not float:
                 raise TypeError("m_a should contain only integers or floats")
-    for row_b in m_b:
-        for i in row_b:
-            if type(i) not in [int, float]:
+            if type(m_b[i]) is not list and type(m_b[i][j]) is not int\
+               and type(m_b[i][j]) is not float:
                 raise TypeError("m_b should contain only integers or floats")
-
-    row_a_len = 0
-    for row_a in m_a:
-        if len(row_a) is not row_a_len and row_a_len is not 0:
-            raise TypeError("each row of m_a must be of the same size")
-        row_a_len = len(row_a)
-    row_b_len = 0
-    for row_b in m_b:
-        if len(row_b) is not row_b_len and row_b_len is not 0:
-            raise TypeError("each row of m_b must be of the same size")
-        row_b_len = len(row_b)
-
-    if row_a_len is not len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    return [[sum(a * b for a, b in zip(row_a, col_b))
-             for col_b in zip(*m_b)] for row_a in m_a]
+            if len(m_a[0]) != len(m_a[i]):
+                raise TypeError("each row of m_a must "
+                                "should be of the same size")
+            if len(m_b[0]) != len(m_b[i]):
+                raise TypeError("each row of m_b must "
+                                "should be of the same size")
+            for k in range(len(m_b)):
+                temp = m_a[i][k] * m_b[k][j]
+                dp += temp
+                if k == len(m_b) - 1:
+                    inside.append(dp)
+                    dp = 0
+            if j == len(m_b[i]) - 1:
+                new.append(inside)
+    return new
