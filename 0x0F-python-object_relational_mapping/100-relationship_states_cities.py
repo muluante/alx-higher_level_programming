4ad9eb6d6447d4cb3c5ef6f484704464380127f9
muluante <muluante@outlook.com>
2023-03-17 07:12:38 -0700

    100-relationship_states_cities

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,7 +11,7 @@ import sys
 if __name__ == "__main__":
     """Connecting"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3])
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
