d3f7acb879299d72d6b3513deb85a1146ff26767
muluante <muluante@outlook.com>
2023-03-20 01:00:26 -0700

    updated

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -24,4 +24,3 @@ class State(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
-
