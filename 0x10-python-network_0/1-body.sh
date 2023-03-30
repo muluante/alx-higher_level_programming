b07da930a0212262150dfd346117336db490b573
muluante <muluante@outlook.com>
2023-03-30 01:03:24 -0700

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# takes in a URL, sends a GET request to the URL and displays the response
+curl -s "$1" -X GET -L
